24b9944b5f02d4e3d4ee7fb2ed11f8556b54eb7b
jserrats <jaume.serrats9@gmail.com>
2025-09-24 16:53:36 +0200

    added ts-warehouse

---
+++ apps/ts-warehouse/src/main.ts
@@ -0,0 +1,40 @@
+import 'dotenv/config'
+import { zigbee, esphome, telegram } from "@ts-assistant/mqtt-assistant"
+import { FloatConveyor } from './conveyors'
+
+
+telegram.info("Starting Warehouse")
+
+const livingrooAirSensor = new zigbee.sensors.air.TH01Z("livingroom_climate_sensor")
+new FloatConveyor(livingrooAirSensor.temperature, "Weather", { room: "living_room" })
+new FloatConveyor(livingrooAirSensor.humidity, "Weather", { room: "living_room" })
+
+const bedroomAirSensor = new zigbee.sensors.air.WSD500A("bedroom_climate_sensor")
+new FloatConveyor(bedroomAirSensor.temperature, "Weather", { room: "bedroom" })
+new FloatConveyor(bedroomAirSensor.humidity, "Weather", { room: "bedroom" })
+
+const studioAirSensor = new zigbee.sensors.air.TH01Z("studio_climate_sensor")
+new FloatConveyor(studioAirSensor.temperature, "Weather", { room: "studio" })
+new FloatConveyor(studioAirSensor.humidity, "Weather", { room: "studio" })
+
+const indoorLivingroomAirSensor = new zigbee.sensors.air.TH01Z("indoor_livingroom_climate_sensor")
+new FloatConveyor(indoorLivingroomAirSensor.temperature, "Weather", { room: "indoor_living_room" })
+new FloatConveyor(indoorLivingroomAirSensor.humidity, "Weather", { room: "indoor_living_room" })
+
+const powerDatacenter = new esphome.SensorESPHome("datacenter-power", "datacenter_power", "W")
+new FloatConveyor(powerDatacenter, "Energy", { room: "datacenter" })
+
+const housePowerSensor = new zigbee.sensors.power.PJ_1203_W("house_power_sensor")
+new FloatConveyor(housePowerSensor.power, "Energy", { room: "home" })
+
+const jaumeWeight = new esphome.SensorESPHome("scale", "weight_jaume", "Kg")
+new FloatConveyor(jaumeWeight, "Body", { person: "Jaume" }, () => { telegram.info(`New weight measure taken \`${jaumeWeight.state}\` Kg`) })
+
+const juditWeight = new esphome.SensorESPHome("scale", "weight_judit")
+new FloatConveyor(juditWeight, "Body", { person: "Judit" }, (sensor) => { telegram.info({ message: `New weight measure taken \`${sensor.state}\` Kg`, recipient: "user" }) })
+
+const studioAirQuality = new esphome.SensorESPHome("airquality", "pm_2_5", "PM 2.5 µg/m3")
+new FloatConveyor(studioAirQuality, "Weather", { room: "studio" })
+
+const studioCO2 = new esphome.SensorESPHome("datacenter", "co2", "CO2 ppm")
+new FloatConveyor(studioCO2, "Weather", { room: "studio" })
