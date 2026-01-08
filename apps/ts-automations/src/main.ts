7fc0ff783a55d8e491973216ee81b05287544d0a
jserrats <jaume.serrats9@gmail.com>
2026-01-08 18:42:35 +0100

    rollback to occupancy sensor in esp

--- apps/ts-automations/src/main.ts
+++ apps/ts-automations/src/main.ts
@@ -46,7 +46,8 @@ musicRemote.on(musicRemote.button.holdBottomClick, () => { musicMoodLight.setOn(
 
 
 // Studio
-const studioPresence = new esphome.BinarySensorESPHome("datacenter", "studio_presence")
+//const studioPresence = new esphome.BinarySensorESPHome("datacenter", "studio_presence")\
+const studioPresence = new zigbee.sensors.presence.IH012_RT01("studio_presence")
 const studioLight = new zigbee.lights.LED1623G12("studio_light")
 const studioFan = new zigbee.switches.E1603("studio_fan")
 const deskPower = new zigbee.switches.E1603("desk_power")
@@ -72,7 +73,7 @@ shelvesLightTimer.on(shelvesLightTimer.events.timeout, () => {
     deskBacklight.setOff()
 })
 
-studioPresence.on(studioPresence.events.state, (state: boolean) => {
+studioPresence.on(studioPresence.occupancy.events.state, (state: boolean) => {
     if (state) {
         deskTimer.cancel();
         shelvesLightTimer.cancel()
