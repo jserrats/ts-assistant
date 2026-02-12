c35d8241207cc84b0c28c4d5c4f9a702595e45df
jserrats <jaume.serrats9@gmail.com>
2026-02-12 18:46:32 +0100

    small fix

--- apps/miqtt/service/install-service.sh
+++ apps/miqtt/service/install-service.sh
@@ -2,3 +2,5 @@ sudo cp miqtt.service /lib/systemd/system/miqtt.service
 sudo chmod 644 /lib/systemd/system/miqtt.service
 sudo systemctl daemon-reload
 sudo systemctl enable miqtt.service
+sudo systemctl start miqtt.service
+sudo systemctl status miqtt.service
