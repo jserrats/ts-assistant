412df569121140efaab9373117d3687368f45df1
jserrats <jaume.serrats9@gmail.com>
2026-02-19 18:46:32 +0100

    updated script

--- apps/miqtt/service/install-service.sh
+++ apps/miqtt/service/install-service.sh
@@ -3,4 +3,3 @@ sudo chmod 644 /lib/systemd/system/miqtt.service
 sudo systemctl daemon-reload
 sudo systemctl enable miqtt.service
 sudo systemctl start miqtt.service
-sudo systemctl status miqtt.service
